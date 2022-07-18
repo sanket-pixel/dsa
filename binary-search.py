0bf201a3b160436b6a12fd8be24f7034c01d651e
Sanket Shah <59618428+sanket-pixel@users.noreply.github.com>
2022-07-18 19:54:11 +0200

    Added cieiling

--- binary-search.py
+++ binary-search.py
@@ -57,7 +57,22 @@ def ceiling(nums, target):
     if s>e :
         return  nums[s]
 
-
+def sqrt(x):
+    arr = []
+    for i in range(x):
+        arr.append(i)
+    s = 0
+    e = len(arr) - 1
+    while (s <= e):
+        m = int((s + e) / 2)
+        if arr[m] * arr[m] < x:
+            s = m + 1
+        elif arr[m] * arr[m] > x:
+            e = m - 1
+        elif arr[m] * arr[m] == x:
+            return arr[m]
+    if (s > e):
+        return arr[e]
 
 
 
